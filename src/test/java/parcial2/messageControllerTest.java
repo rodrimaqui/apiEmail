97c4ee41722ab51a7543ab8a69b904ed0d348b75
rodrimaqui <rodri.maqui@hotmail.com>
2017-06-17 03:47:28 -0300

    Vamos a ver que onda

--- src/test/java/parcial2/messageControllerTest.java
+++ src/test/java/parcial2/messageControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {App.class})
 @WebAppConfiguration
+@ActiveProfiles("default")
 public class messageControllerTest extends TestCase {
 
 
--- src/test/java/parcial2/userControllerTest.java
+++ src/test/java/parcial2/userControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,6 +39,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {App.class})
 @WebAppConfiguration
+@ActiveProfiles("default")
 public class userControllerTest extends TestCase{
 
     @Autowired
